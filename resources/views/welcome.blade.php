2d7f7c0262c30062d6a4338e8013cfcf8524b255
John Evans <john@grandadevans.com>
2021-07-07 02:16:34 +0100

    Update readme and welcome pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,9 +68,9 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a class="login-link" href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a class="login-link" href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
                             <a class="registration-link"  href="{{ route('register') }}">Register</a>
@@ -81,18 +81,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Banking App
+                </div>
+
+                <div class="m-b-md">
+                    Powered by Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laravel.com/docs">Laravel Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel-news.com">Laravel News</a>
+                    <a href="https://blog.laravel.com">Laravel Blog</a>
                 </div>
             </div>
         </div>
