370a192ca6ebf2d0a6bf24a8d603bb7b3c29711e
John Evans <john@grandadevans.com>
2021-07-07 02:12:32 +0100

    Add BaseSeeder to enable bare setup

---
+++ database/seeders/BaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Bank\Models\Provider;
+use Illuminate\Database\Seeder;
+
+class BaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(PaymentMethodSeeder::class);
+
+        Provider::factory()->create([
+            'name' => 'N/A',
+            'payment_method_id' => 7
+        ]);
+
+    }
+}
