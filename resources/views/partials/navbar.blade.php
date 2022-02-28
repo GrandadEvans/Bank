f774eae0c18e4ebb0034003323933bb2f6dbe368
John Evans <john@grandadevans.com>
2022-02-28 21:18:31 +0000

    Tidy navbar conditionals up

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,31 +3,39 @@
         Banking App
     </a>
 
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
+            data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
 
-    <ul class="navbar-nav px-3">
-
     @auth
-    <li class="nav-item px-3 dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <ul class="navbar-nav px-3">
+            <li class="nav-item px-3 dropdown">
+                <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown1"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false">
+
                     Hi, {{ auth()->user()->name }}, see your Account
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                     <a class="dropdown-item" href="{{ route('get_logout') }}">Logout</a>
                 </div>
             </li>
-    </ul>
+        </ul>
     @endauth
 
     @guest
         <ul class="navbar-nav px-3">
-
-        <li class="nav-item px-3 dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item px-3 dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
                     Login/Register
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
@@ -36,5 +44,5 @@
                 </div>
             </li>
         </ul>
-        @endguest
+    @endguest
 </nav>
