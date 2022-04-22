08343ce1fce1fb6c1fddacd52f578dbd3635de9a
John Evans <john@grandadevans.com>
2022-04-22 09:43:09 +0100

    All tests green in Jest, Codeception & Cypress. Closing #38

--- resources/js/includes/bootstrap.js
+++ resources/js/includes/bootstrap.js
@@ -14,7 +14,6 @@ window.Swal   = window.swal = Swal;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-const apiKey = bugSnagKey();
 if (typeof apiKey === 'string') {
     Bugsnag.start({
         apiKey: '5654e4b0844b36dc2e56d534571d8230',
